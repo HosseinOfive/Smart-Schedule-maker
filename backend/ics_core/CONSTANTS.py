d81e316270db6a2fcf16d3c6755b783ff03f9b6f
HosseinOfive <h.p.nejatian@gmail.com>
2026-01-01 16:57:39 -0500

    Added the time validatory functions and method THIS IS A BETA

--- backend/ics_core/CONSTANTS.py
+++ backend/ics_core/CONSTANTS.py
@@ -5,3 +5,6 @@ EXPANSION_MONTHS = 3
 DEFAULT_DAY_START_HOUR = 8  # 8 AM
 DEFAULT_DAY_END_HOUR = 22   # 10 PM
 MIN_FREE_SLOT_MINUTES = 30  # Minimum duration for a free slot to be considered valid
+FIXED_TRAVEL_TIME_MINUTES = 15   ##THIS is the fixed travel time we add between activities 
+####IT IS FOR STUB
+##later we implement DURATION ESTIMATION VIA MAPBOX 
